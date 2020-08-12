2380a0337aca4727c16fd19b87da91ad8bbe7cb7
Heeuul <46469679+Heeuul@users.noreply.github.com>
2020-08-12 14:14:35 +0800

    v0.1
    player inserted and moveable

---
+++ Project/Project/Character.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project
+{
+    public class Character : GameObject
+    {
+        Texture2D chartex;
+        string charTexName;
+
+        public Character()
+        {
+            charTexName = "player";
+        }
+
+        public override void Initialize()
+        {
+            chartex = Game1.Assets[charTexName];
+            position = new Vector2(Game1.Screen.ClientBounds.Width / 2.0f,
+                Game1.Screen.ClientBounds.Height / 2.0f);
+            origin = new Vector2(chartex.Width / 2.0f, chartex.Height / 2.0f);
+            alive = true;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MouseState mouse = Mouse.GetState();
+
+            heading = mouse.Position.ToVector2() - position;
+            position += heading * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(chartex, position, origin:origin);
+        }
+    }
+}
---
+++ Project/Project/GameObject.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project
+{
+    public abstract class GameObject
+    {
+        public bool alive;
+        public string name;
+        public Vector2 position;
+        public Vector2 origin;
+        public Vector2 heading;
+
+        protected GameObject()
+        {
+            name = string.Empty;
+            alive = false;
+            position = Vector2.Zero;
+            origin = Vector2.Zero;
+            heading = new Vector2(1f, 0f);
+        }
+
+        public abstract void Initialize();
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+
+
+    }
+}
