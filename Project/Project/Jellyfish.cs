6f324aa9ae29b8605c43dc6b7a61e89e51df6dc1
Heeuul <1181302192@student.mmu.edu.my>
2020-10-12 04:38:37 +0800

    2.5.1
    + Added classes for other creatures

---
+++ Project/Project/Jellyfish.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Project
+{
+    class Jellyfish : Enemy
+    {
+        public Jellyfish()
+        {
+            name = "jellyfish";
+            gameSize = 1;
+            speed = 1;
+            location = 2;
+        }
+        public override void PatternMovement(GameTime gameTime)
+        {
+            FlockingMovement(gameTime, World.stingray);
+        }
+    }
+}
---
+++ Project/Project/Turtle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Project
+{
+    class Turtle : Enemy
+    {
+        public Turtle()
+        {
+            name = "turtle";
+            gameSize = 3;
+            speed = 2;
+            location = 1;
+        }
+        public override void PatternMovement(GameTime gameTime)
+        {
+            FlockingMovement(gameTime, World.stingray);
+        }
+    }
+}
